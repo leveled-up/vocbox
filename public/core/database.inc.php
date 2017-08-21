b851999560efdedf0e29036c3d8787016020dd53
Anthony Yujay <anthony.yujay@runstorage.com>
2017-08-21 14:58:44 +0200

    Update public/

--- public/core/database.inc.php
+++ public/core/database.inc.php
@@ -27,6 +27,7 @@ function query_user_create($user_info) {
     $query .= "'".encode($vq)."', '";
 
   $query = "INSERT INTO users VALUES($query'$last_sign_in');";
+  echo $query;
   return $query;
 
 }
