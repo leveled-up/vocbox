9c69b4755abb6a3b82e83c204761c85bd665516b
Anthony Yujay <anthony.yujay@runstorage.com>
2017-08-21 17:48:41 +0200

    Update public/

--- public/index.php
+++ public/index.php
@@ -8,5 +8,39 @@ basic_template();
   Welcome to VocBox, the Vocabulary Trainer that's focused on Machine Learning and AI. Thanks for being here. &hearts;
 </p>
 
+<a href="/new_library" id="index_btn_library_new">
+  <i class="fa fa-add"></i>
+  New Library
+</a>
 
-<?php
+<table class="table table-striped" id="index_table_libraries">
+  <thead>
+    <th>Languages</th>
+    <th>Comment</th>
+    <th>Actions</th>
+  </thead>
+  <tbody>
+    <?php
+      $libraries_list_query = query_library_list($user);
+      $libraries_list = query($libraries_list_query, false);
+
+      if(count($libraries_list < 1))
+        echo "
+        <tr>
+          <td>You don't have any libraries yet.</td>
+          <td></td>
+          <td></td>
+        </tr>";
+      else
+        foreach($libraries_list as $library)
+          echo "
+          <tr>
+            <td>".format_laguages($library["langs"])."</td>
+            <td>
+              <i>".htmlentities($library["comment"])."</i>
+            </td>
+            <td></td>
+          </tr>";
+    ?>
+  </tbody>
+</table>
