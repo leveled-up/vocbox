cc0c2c63075cb37a12538d4526de0563fe1b7ccb
Anthony Yujay <anthony.yujay@runstorage.com>
2017-08-21 12:41:24 +0200

    Update .travis.yml and commit.sh

--- commit.sh
+++ commit.sh
@@ -3,3 +3,6 @@ git commit -m "$1"
 git push
 DATE=`date +%s`
 zip .zip/vocbox-backup-$DATE.zip functions/* public/* .travis.yml README.md
+coffee -cbo .out/ public/
+cp public/* .out/
+rm .out/*.coffee
