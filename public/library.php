32dd9c2d8b18006f4d9062d03d89449a6de8ca8d
Anthony Yujay <anthony.yujay@runstorage.com>
2017-08-22 11:38:31 +0200

    Update public/

--- public/library.php
+++ public/library.php
@@ -4,13 +4,13 @@ include("core/core.inc.php");
 
 // Read Parameter
 $current_libary = $_GET["library"];
-echo $current_libary;
 if(!is_numeric($current_libary))
   exit("Error 404: The library ID provided cannot exist.");
 
 // Get Info from Database
 $library_info_query = query_library_info($current_library, $user);
 $library_info = query($library_info_query);
+echo $library_info_query; var_dump($libary_info);
 if(!isset($library_info["id"]))
   exit("Error 404: The library {$current_library} was not found this user.");
 
