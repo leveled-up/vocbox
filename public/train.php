c0f5efc2bebcee9a4ae1faafbbf9af6a615d5923
Anthony Yujay <anthony.yujay@runstorage.com>
2017-09-29 19:23:06 +0200

    Update train.php

--- public/train.php
+++ public/train.php
@@ -132,7 +132,7 @@ elseif(isset($_GET["action:yt_justnow"])) {
               </td>
             </tr>";
     }
-  echo "</table><br/><br/>
+  echo "</table><br/>
     <center>
       <a href=\"https://www.youtube.com/feed/trending\">
 	     <img src=\"https://developers.google.com/youtube/images/developed-with-youtube-sentence-case-dark.png\" height=\"80px\" />
